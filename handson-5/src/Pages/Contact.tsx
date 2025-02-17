ed8bd97f52bcca85b192bc62ed73dacb9f38d7f0
joshua sinabutar <joshuasinabutar7@gmail.com>
2025-02-17 11:13:56 +0700

    Selesai

--- handson-5/src/Pages/Contact.tsx
+++ handson-5/src/Pages/Contact.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Contact = () => {
+    const navigate = useNavigate();     
     return(
-        <div>Contact Page</div>
+        <div>
+            <h1>Contact Page</h1>
+            <div className="contact-form">
+                <button onClick={() => navigate("form")}>Contact form</button>
+                <button onClick={() => navigate("info")}>Contact info</button>
+            </div>
+        </div>
     )
 }
 
